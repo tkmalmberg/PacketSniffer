c6a171aefee10532297a42abd6424dfc2c43f4c4
Tyler Malmberg <tmalmber@cord.edu>
2020-03-17 12:16:47 -0500

    Basic Snffer made
    Only works in linux rn lol

--- sniffer.py
+++ sniffer.py
@@ -1,14 +1,29 @@
 import socket
 import struct
-import binascii
+import textwrap
 
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.htons(0x0800))
+def main():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
-while True:
-    packet = s.recvfrom(2048)
-    ethernet_header = packet[0][0:14]
-    eth_header = struct.unpack("!6s6s2s", ethernet_header)
-    print("Destination MAC:" + binascii.hexlify(eth_header[0]) + " Source MAC:" + binascii.hexlify(eth_header[1]) + " Type:" + binascii.hexlify(eth_header[2]))
-    ipheader = pkt[0][14:34]
-    ip_header = struct.unpack("!12s4s4s", ipheader)
-    print("Source IP:" + socket.inet_ntoa(ip_header[1]) + " Destination IP:" + socket.inet_ntoa(ip_header[2]))
+    while True:
+        raw_data, addr = conn.recvfrom(65535)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\n Ethernet Frame:')
+        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+
+# Unpacks ethernet frame
+# Passes in the full packet, and unpacks the Destination, Source, and the Type of the
+# packet.
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:] 
+
+# Returns a properly formatted MAC address
+# Passes in a byte address and makes it readable for a human
+# (i.e. AA:BB:CC:DD:EE:FF)
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    return ':'.join(bytes_str).upper()
+
+
+main()
